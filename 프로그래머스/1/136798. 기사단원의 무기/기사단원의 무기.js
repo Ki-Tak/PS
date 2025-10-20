3ab59f2ef8148fd122ef4213f196f86a3a385cf6
KiTak Lee <87828811+Ki-Tak@users.noreply.github.com>
2025-10-20 16:39:02 +0900

    [level 1] Title: 기사단원의 무기, Time: 144.32 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,19 @@
+function solution(number, limit, power) {
+    let sum=0;
+    for(let i=1;i<=number;i++){
+        let count=0
+        for(let j=1;j*j<=i;j++){
+            if(j*j===i){
+                count++
+            }
+            else if(i%j===0){
+                count+=2
+            }
+            if(count>limit){
+                break
+            }
+        }
+        count>limit ? sum+=power : sum+=count
+    }
+    return sum;
+}
