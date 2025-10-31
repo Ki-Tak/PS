d4b8ece68c70dc1715c47b66c60ed98bf2abfd58
KiTak Lee <87828811+Ki-Tak@users.noreply.github.com>
2025-10-31 16:14:43 +0900

    [level 1] Title: 콜라 문제, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,8 @@
 function solution(a, b, n) {
     var answer = 0;
     while (Math.floor(n/a)!==0){
-        let temp= n%a
-        n=Math.floor(n/a)*b
-        answer+=n
-        n+=temp
+        answer += Math.floor(n/a)*b;
+        n= Math.floor(n/a)*b +n%a 
     }
     return answer;
 }
