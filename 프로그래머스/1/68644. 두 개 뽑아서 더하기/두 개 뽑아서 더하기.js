258e587c02b4e900bc44f1752daf14d9f3354c58
KiTak Lee <87828811+Ki-Tak@users.noreply.github.com>
2025-11-01 18:11:17 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const numberSet = new Set()
+    for(let i=0;i<numbers.length-1;i++){
+        for(let j=i+1;j<numbers.length;j++){
+            numberSet.add(numbers[i]+numbers[j])
+        }
+    }
+    return [...numberSet].sort((a,b)=>a-b);
+}
